b0e8de12d6ef14443aa8782e52573741e0fece81
zhangyilun <zhangyilun.0719@gmail.com>
2017-10-05 16:53:32 -0400

    bug fix

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = "jose"
+app.secret_key = "yilunzhang"
 api = Api(app)
 
 
